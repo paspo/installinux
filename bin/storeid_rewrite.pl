1d2574fc1eb73b2914e33d7d80b2832403c0b8eb
Paolo Asperti <paolo@asperti.com>
2016-05-12 22:55:37 +0200

    FEATURE: use of regex for storeid rewriting

--- bin/storeid_rewrite.pl
+++ bin/storeid_rewrite.pl
@@ -5,35 +5,42 @@ use warnings;
 use autodie;
 use utf8;
 
-my %urls;
-my $dir = '/etc/installinux/mirrors';
+my @rules;
+my $dir = '/etc/squid3/storeid';
 
 opendir (DIR, $dir) or die $!;
 while (my $file = readdir(DIR)) {
     next if ($file =~ m/^\./);
     open (my $data, '<', $dir . '/' . $file) || die "Error: $!\n";
-    while (my $line = <$data>) {
-        chomp $line;
-        $line =~ s/\R//g;
-        next if ($line =~ m/^\s*#?$/);
-        my($mirror_url, $redir_url) = split("\t", $line);
-        $urls{$mirror_url} = $redir_url;
+    while (<$data>) {
+        chomp;
+		if (/^\s*([^\t]+?)\s*\t+\s*([^\t]+?)\s*$/) {
+        	push(@rules, [qr/$1/, $2]);
+        } else {
+        	print STDERR "$0: Parse error in $ARGV[0] (line $.)\n";
+        }
     }
 }
 closedir(DIR);
 
-$| = 1;
-while (<>) {
+print STDERR "$0: loaded " . scalar @rules . " rules\n";
+
+URL: while (<STDIN>) {
     chomp;
     my @X = split(" ");
     my $a = $X[0];
-    my $url = $X[1];
-    my $new_URL = $url;
+	$_ = $X[1];
+    my $new_URL = $X[1];
 
-    while( my( $key, $value ) = each %urls ){
-        if (index($url, $key) != -1) {
-            $new_URL =~ s/$key/$value/g;
-        }
+    foreach my $rule (@rules) {
+        if (my @match = /$rule->[0]/) {
+            $_ = $rule->[1];
+			for (my $i=1; $i<=scalar(@match); $i++) {
+            	s/\$$i/$match[$i-1]/g;
+            }
+            print "$a OK store-id=$_\n";
+            next URL;
+    	}
     }
 
     print "$a OK store-id=$new_URL\n";
